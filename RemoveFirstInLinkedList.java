c99160ca0c6ad2805499132c98a2ce1e395e588d
Yashwant Kumar <53390920+itsyashh@users.noreply.github.com>
2020-09-07 17:51:30 +0530

    Add files via upload

---
+++ RemoveFirstInLinkedList.java
@@ -0,0 +1,69 @@
+package LearnLinkedList;
+import java.util.Scanner;
+
+public class RemoveFirstInLinkedList {
+
+    public static class Node {
+        int data;
+        Node next;
+    }
+
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int val) {
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+
+            if (size == 0) {
+                head = tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+
+            size++;
+        }
+
+        public int size(){
+            return size;
+        }
+
+        public void display(){
+            for(Node temp = head; temp != null; temp = temp.next){
+                System.out.print(temp.data + " ");
+            }
+            System.out.println();
+        }
+
+        public void removeFirst(){
+            // write your code here
+            if(size == 0){
+                System.out.println("list is empty");
+            }else if(size == 1){
+                head = tail = null;
+                size = 0;
+            }else {
+                head = head.next;
+                size--;
+            }
+        }
+    }
+
+    public static void main (String[] args){
+        Scanner scn = new Scanner(System.in);
+        DisplayLinkedList.LinkedList list = new DisplayLinkedList.LinkedList();
+
+        String str = scn.next();
+        while(str.equals("quit") == false){
+            if(str.startsWith("addLast")){
+                int val = Integer.parseInt(str.split(" ")[1]);
+                list.addLast(val);
+            }
+            str = scn.next();
+        }
+    }
+}
